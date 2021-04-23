ed854c0529e4a19481625d2feb336c04fc55804c
Jon Haddow <jon.haddow@cdsm.co.uk>
2021-04-23 20:38:29 +0100

    Add static images to graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,14 @@ module.exports = {
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
-				path: `./src/config`,
+				path: `${__dirname}/static/img`,
+				name: "images",
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/config`,
 				name: "config",
 			},
 		},
