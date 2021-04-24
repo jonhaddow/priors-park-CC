85819bdc05c82443cfe9d274960ff76a09eeb965
Jon Haddow <jon.haddow@cdsm.co.uk>
2021-04-24 20:53:22 +0100

    Moving around order of plugins - https://github.com/gatsbyjs/gatsby/issues/13469

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,36 @@ module.exports = {
 		description: "The heart of the community",
 	},
 	plugins: [
-		"gatsby-plugin-typescript",
-		`gatsby-plugin-eslint`,
-		"gatsby-plugin-react-helmet",
-		"gatsby-plugin-postcss",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-transformer-remark",
+			options: {
+				plugins: [
+					{
+						resolve: "gatsby-remark-relative-images",
+						options: {
+							name: "uploads",
+						},
+					},
+					{
+						resolve: "gatsby-remark-images",
+						options: {
+							// It's important to specify the maxWidth (in pixels) of
+							// the content container as this plugin uses this as the
+							// base for generating different widths of each image.
+							maxWidth: 2048,
+						},
+					},
+					{
+						resolve: "gatsby-remark-copy-linked-files",
+						options: {
+							destinationDir: "static",
+						},
+					},
+				],
+			},
+		},
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
@@ -43,36 +69,10 @@ module.exports = {
 				name: "sermons",
 			},
 		},
-		"gatsby-plugin-sharp",
-		"gatsby-transformer-sharp",
-		{
-			resolve: "gatsby-transformer-remark",
-			options: {
-				plugins: [
-					{
-						resolve: "gatsby-remark-relative-images",
-						options: {
-							name: "uploads",
-						},
-					},
-					{
-						resolve: "gatsby-remark-images",
-						options: {
-							// It's important to specify the maxWidth (in pixels) of
-							// the content container as this plugin uses this as the
-							// base for generating different widths of each image.
-							maxWidth: 2048,
-						},
-					},
-					{
-						resolve: "gatsby-remark-copy-linked-files",
-						options: {
-							destinationDir: "static",
-						},
-					},
-				],
-			},
-		},
+		"gatsby-plugin-typescript",
+		`gatsby-plugin-eslint`,
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-postcss",
 		"gatsby-plugin-netlify-cms",
 		"gatsby-plugin-netlify", // make sure to keep it last in the array
 	],
