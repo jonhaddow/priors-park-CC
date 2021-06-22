04e46843ffc49758fa2570f1764010f92e4b58f6
Jon Haddow <haddowjon@protonmail.com>
2021-06-22 20:59:33 +0100

    Add mobile navigation menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSections } from "../hooks";
+import { GiHamburgerMenu } from "@react-icons/all-files/gi/GiHamburgerMenu";
 
 interface NavigationProps {
 	navRef: React.MutableRefObject<HTMLElement | null>;
@@ -15,14 +16,26 @@ export const Navigation: React.FC<NavigationProps> = ({
 		id: string;
 	}[] = [{ title: "Home", id: "home" }, ...useSections()];
 
+	const [expand, setExpand] = useState(false);
+
 	return (
-		<nav ref={navRef} className="bg-primary fixed w-full hidden md:block z-50">
-			<ul className="flex justify-start flex-wrap z-50">
+		<nav ref={navRef} className="bg-primary fixed w-full md:block z-50">
+			<button
+				className="md:hidden float-right p-4"
+				onClick={() => setExpand(!expand)}
+			>
+				<GiHamburgerMenu color="white" />
+			</button>
+			<ul
+				className={`${
+					expand ? "block" : "hidden"
+				} mt-8 md:mt-0 md:flex justify-start flex-wrap z-50`}
+			>
 				{sections.map((x) => (
-					<li key={x.id} className="p-4">
+					<li key={x.id} className="md:p-4">
 						<button
 							onClick={() => onNavigate(x.id)}
-							className="text-gray-50 rounded-lg p-2 hover:text-white hover:bg-primary-dark transition-colors focus:outline-none focus:bg-primary-dark focus:text-white"
+							className="w-full md:w-auto text-gray-50 md:rounded-lg p-2 hover:text-white hover:bg-primary-dark transition-colors focus:outline-none focus:bg-primary-dark focus:text-white"
 						>
 							{x.title}
 						</button>
