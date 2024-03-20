989c870acb19a7012a96972cf9d6d78f20384372
Jon Haddow <jon@haddow.me>
2024-03-20 19:24:44 +0000

    Fix import statement for sanityIntegration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-import { sanityIntegration } from "@sanity/astro";
+import sanityIntegration from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
