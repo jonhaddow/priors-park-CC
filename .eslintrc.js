589cdc67f1b4ed4442168da9c19c200eef258e27
Jon Haddow <jon@haddow.me>
2024-05-07 20:24:54 +0100

    Set up lint step in GHA build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-	extends: [
-		"jonhaddow",
-		"jonhaddow/react",
-		"jonhaddow/typescript",
-		"plugin:tailwindcss/recommended",
-	],
-	parserOptions: {
-		sourceType: "module",
+  extends: [
+    "jonhaddow",
+    "jonhaddow/react",
+    "jonhaddow/typescript",
+    "plugin:tailwindcss/recommended",
+  ],
+  parserOptions: {
+    sourceType: "module",
 
-		// The include property on this file is used to determine
-		// the files to lint.
-		project: "./tsconfig.json",
-	},
+    // The include property on this file is used to determine
+    // the files to lint.
+    project: "./tsconfig.json",
+  },
 };
