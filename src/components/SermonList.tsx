51fa29f1cff6027207211376bb4006cf6326cd6e
Jon Haddow <jon@haddow.me>
2025-07-24 20:40:24 +0100

    Use live data for sermons

--- src/components/SermonList.tsx
+++ src/components/SermonList.tsx
@@ -4,15 +4,18 @@ import BackgroundShade from "./BackgroundShade";
 import Plyr from "plyr";
 import "plyr/dist/plyr.css";
 import { useLayoutEffect, useRef } from "preact/hooks";
+import type { Sermon } from "types";
 
 function Sermon({
   title,
   date,
   description,
+  fileURL,
 }: {
   title: string;
   date: string;
-  description: string;
+  description?: string;
+  fileURL: string;
 }) {
   const ref = useRef<HTMLAudioElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
@@ -33,44 +36,34 @@ function Sermon({
         <h3 className="text-text-primary text-2xl font-bold">{title}</h3>
         <p className="text-text-secondary text-sm">{date}</p>
       </div>
-      <div className="flex flex-col">
-        <h3 className="text-text-secondary text-md">{description}</h3>
-      </div>
+      {description && (
+        <div className="flex flex-col">
+          <h3 className="text-text-secondary text-md">{description}</h3>
+        </div>
+      )}
       <div
         ref={wrapperRef}
         className="flex w-full flex-row items-center gap-4 rounded-2xl opacity-0 transition-opacity duration-300"
       >
         <audio ref={ref} className="">
-          <source src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
+          <source src={fileURL} />
         </audio>
       </div>
     </li>
   );
 }
 
-function SermonList() {
+function SermonList({ sermons }: { sermons: Sermon[] }) {
   return (
     <BackgroundShade
       color="green"
       direction="left"
-      className="grid min-h-screen place-items-center"
+      className="grid flex-grow place-items-center"
     >
       <ul className="flex max-w-2xl flex-col gap-8 px-4 md:px-16">
-        <Sermon
-          title="Sermon 1"
-          date="2021-01-01"
-          description="Description 1"
-        />
-        <Sermon
-          title="Sermon 2"
-          date="2021-01-02"
-          description="Description 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.Description 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
-        />
-        <Sermon
-          title="Sermon 3"
-          date="2021-01-03"
-          description="Description 3"
-        />
+        {sermons.map((sermon) => (
+          <Sermon key={sermon._id} {...sermon} date={sermon.publishedDate} />
+        ))}
       </ul>
     </BackgroundShade>
   );
