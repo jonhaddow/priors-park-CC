304cb0ecb85124aee89c56369b1297676e26b10b
Jon Haddow <jon@haddow.me>
2025-08-30 21:24:26 +0100

    Add network links

--- src/components/OurStory.tsx
+++ src/components/OurStory.tsx
@@ -27,41 +27,63 @@ function Person({ name, title }: { name: string; title: string }) {
   );
 }
 
+function NetworkLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: ComponentChildren;
+}) {
+  return (
+    <li>
+      <a className="underline hover:no-underline" href={href}>
+        {children}
+      </a>
+    </li>
+  );
+}
+
 function OurStory() {
   return (
     <FadeReveal>
-      <div className="bg-light-background flex flex-col gap-16 px-8 md:flex-row lg:px-32">
-        <Section>
-          <SectionHeader>Our mission</SectionHeader>
-          <div className="text-text-secondary flex flex-col gap-2 text-lg">
-            <p className="italic">
-              We are a local church committed to building authentic community,
-              sharing life together, and following Jesus in everyday ways.
-            </p>
-            <p>
-              We offer many different types of support to our local community
-              including our compassion cafe.
-            </p>
-            <p>
-              We are part of a network of{" "}
-              <a className="underline" href="https://fiec.org.uk/">
-                FIEC (Fellowship of independent evangelical Churches)
-              </a>
-              .
-            </p>
-          </div>
-        </Section>
-        <Section>
-          <SectionHeader>Our team</SectionHeader>
-          <div className="text-text-secondary text-md">
-            <ul className="grid grid-cols-1 gap-4 @md:grid-cols-2">
-              <Person name="Nigel" title="Pastor" />
-              <Person name="Jackie" title="Pastoral Support Worker" />
-              <Person name="Vanessa" title="Children's leader" />
-              <Person name="Tammie" title="Kitchen and cafe manager" />
-            </ul>
-          </div>
-        </Section>
+      <div className="bg-light-background px-8 lg:px-32">
+        <div className="flex flex-col gap-16 md:flex-row">
+          <Section>
+            <SectionHeader>Our mission</SectionHeader>
+            <div className="text-text-secondary flex flex-col gap-2 text-lg">
+              <p className="italic">
+                We are a local church committed to building authentic community,
+                sharing life together, and following Jesus in everyday ways.
+              </p>
+              <p>
+                We offer many different types of support to our local community
+                including our compassion cafe.
+              </p>
+            </div>
+            <SectionHeader>Network</SectionHeader>
+            <div className="text-text-secondary text-md">
+              <ul className="ml-4 list-disc">
+                <NetworkLink href="https://fiec.org.uk/">
+                  FIEC (Fellowship of independent evangelical Churches)
+                </NetworkLink>
+                <NetworkLink href="https://tewkesbury.church/">
+                  Tewkesbury Churches Together
+                </NetworkLink>
+              </ul>
+            </div>
+          </Section>
+          <Section>
+            <SectionHeader>Our team</SectionHeader>
+            <div className="text-text-secondary text-md">
+              <ul className="grid grid-cols-1 gap-4 @md:grid-cols-2">
+                <Person name="Nigel" title="Pastor" />
+                <Person name="Jackie" title="Pastoral Support Worker" />
+                <Person name="Vanessa" title="Children's leader" />
+                <Person name="Tammie" title="Kitchen and cafe manager" />
+              </ul>
+            </div>
+          </Section>
+        </div>
       </div>
     </FadeReveal>
   );
